468905ab3448a096e61c2032058716f97dfdd209
ESR <82104698+esrdapp@users.noreply.github.com>
2021-09-08 13:13:43 +0100

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,12 +1,15 @@
-const TokenEth = artifacts.require('TokenHpb.sol');
+const TokenHpb = artifacts.require('TokenHpb.sol');
+const BridgeHpb = artifacts.require('BridgeHpb.sol');
+
 const TokenBsc = artifacts.require('TokenBsc.sol');
-const BridgeEth = artifacts.require('BridgeHpb.sol');
 const BridgeBsc = artifacts.require('BridgeBsc.sol');
 
 module.exports = async function (deployer, network, addresses) {
   if(network === 'hpb') {
     await deployer.deploy(TokenHpb);
     const tokenHpb = await TokenHpb.deployed();
+    
+    //mint 1000 test tokens
     await tokenHpb.mint(addresses[0], 1000);
     await deployer.deploy(BridgeHpb, tokenHpb.address);
     const bridgeHpb = await BridgeHpb.deployed();
