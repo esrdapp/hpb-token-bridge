bd27fd83720dba0cdc9cc2000bbe6e9cff32d5bb
ESR <82104698+esrdapp@users.noreply.github.com>
2021-09-08 15:19:58 +0100

    Update hpb-bsc-bridge.js

--- scripts/hpb-bsc-bridge.js
+++ scripts/hpb-bsc-bridge.js
@@ -2,8 +2,12 @@ const Web3 = require('web3');
 const BridgeHpb = require('../build/contracts/BridgeHpb.json');
 const BridgeBsc = require('../build/contracts/BridgeBsc.json');
 
-// define web sockets
+// define connections
+
+//HPB Web socket
 const web3Hpb = new Web3('ws://ws.hpbnode.com');
+
+//BSC Testnet Node
 const web3Bsc = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545');
 
 const adminPrivKey = 'add your private key here, eg 0xABCDEF....';
