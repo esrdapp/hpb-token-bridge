afbf939ac81c53661acb43aeec45026b79a18cbb
ESR <82104698+esrdapp@users.noreply.github.com>
2021-09-08 13:25:21 +0100

    Update eth-token-balance.js

--- scripts/eth-token-balance.js
+++ scripts/eth-token-balance.js
@@ -1,9 +1,9 @@
-const TokenEth = artifacts.require('./TokenEth.sol');
+const TokenHpb = artifacts.require('./TokenHpb.sol');
 
 module.exports = async done => {
   const [sender, _] = await web3.eth.getAccounts();
-  const tokenEth = await TokenEth.deployed();
-  const balance = await tokenEth.balanceOf(sender);
+  const tokenHpb = await TokenHpb.deployed();
+  const balance = await tokenHpb.balanceOf(sender);
   console.log(balance.toString());
   done();
 }
