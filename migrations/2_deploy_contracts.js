c680117af9f115cbcf5dbb12604983289c4633f8
ESR <82104698+esrdapp@users.noreply.github.com>
2021-09-08 13:09:37 +0100

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,16 +1,16 @@
-const TokenEth = artifacts.require('TokenEth.sol');
+const TokenEth = artifacts.require('TokenHpb.sol');
 const TokenBsc = artifacts.require('TokenBsc.sol');
-const BridgeEth = artifacts.require('BridgeEth.sol');
+const BridgeEth = artifacts.require('BridgeHpb.sol');
 const BridgeBsc = artifacts.require('BridgeBsc.sol');
 
 module.exports = async function (deployer, network, addresses) {
   if(network === 'hpb') {
-    await deployer.deploy(TokenEth);
-    const tokenEth = await TokenEth.deployed();
-    await tokenEth.mint(addresses[0], 1000);
-    await deployer.deploy(BridgeEth, tokenEth.address);
-    const bridgeEth = await BridgeEth.deployed();
-    await tokenEth.updateAdmin(bridgeEth.address);
+    await deployer.deploy(TokenHpb);
+    const tokenHpb = await TokenHpb.deployed();
+    await tokenHpb.mint(addresses[0], 1000);
+    await deployer.deploy(BridgeHpb, tokenHpb.address);
+    const bridgeHpb = await BridgeHpb.deployed();
+    await tokenHpb.updateAdmin(bridgeHpb.address);
   }
   if(network === 'bscTestnet') {
     await deployer.deploy(TokenBsc);
