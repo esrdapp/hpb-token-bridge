a17f1c483686e0549f2ab78c9150445531899c82
ESR <82104698+esrdapp@users.noreply.github.com>
2021-09-08 13:19:22 +0100

    Create bsc-hpb-bridge.js

---
+++ scripts/bsc-hpb-bridge.js
@@ -0,0 +1,53 @@
+const Web3 = require('web3');
+const BridgeHpb = require('../build/contracts/BridgeHpb.json');
+const BridgeBsc = require('../build/contracts/BridgeBsc.json');
+
+// define web sockets
+const web3Hpb = new Web3('ws://ws.hpbnode.com');
+const web3Bsc = new Web3('https://data-seed-prebsc-1-s1.binance.org:8545');
+
+const adminPrivKey = 'add your private key here, eg 0xABCDEF....';
+
+// forwarder to HPB network
+const { address: admin } = web3Hpb.eth.accounts.wallet.add(adminPrivKey);
+
+const bridgeBsc = new web3Bsc.eth.Contract(
+  BridgeEth.abi,
+  BridgeEth.networks['269'].address
+);
+
+const bridgeBsc = new web3Bsc.eth.Contract(
+  BridgeBsc.abi,
+  BridgeBsc.networks['97'].address
+);
+
+bridgeEth.events.Transfer(
+  {fromBlock: 0, step: 0}
+)
+.on('data', async event => {
+  const { from, to, amount, date, nonce, signature } = event.returnValues;
+
+  const tx = bridgeBsc.methods.mint(from, to, amount, nonce, signature);
+  const [gasPrice, gasCost] = await Promise.all([
+    web3Bsc.eth.getGasPrice(),
+    tx.estimateGas({from: admin}),
+  ]);
+  const data = tx.encodeABI();
+  const txData = {
+    from: admin,
+    to: bridgeBsc.options.address,
+    data,
+    gas: gasCost,
+    gasPrice
+  };
+  const receipt = await web3Bsc.eth.sendTransaction(txData);
+  console.log(`Transaction hash: ${receipt.transactionHash}`);
+  console.log(`
+    Processed transfer:
+    - from ${from} 
+    - to ${to} 
+    - amount ${amount} tokens
+    - date ${date}
+    - nonce ${nonce}
+  `);
+});
