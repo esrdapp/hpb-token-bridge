74daf495b85ae0ffcb648ea4c5867dba57678ec8
ESR <82104698+esrdapp@users.noreply.github.com>
2021-09-08 13:10:27 +0100

    Update TokenEth.sol

--- contracts/TokenEth.sol
+++ contracts/TokenEth.sol
@@ -2,6 +2,6 @@ pragma solidity ^0.8.0;
 
 import './TokenBase.sol';
 
-contract TokenEth is TokenBase {
-  constructor() TokenBase('ETH Token', 'ETK') {}
+contract TokenHpb is TokenBase {
+  constructor() TokenBase('HPB Token', 'HTK') {}
 }
